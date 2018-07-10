912ecca5b08592a6770b86c937c86a87ec0ac7d9
Harley McNerthney <harley.mcnerthney@live.com>
2018-07-10 12:37:02 -0700

    Base

--- CashRegister/Program.cs
+++ CashRegister/Program.cs
@@ -4,6 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* 
+    The 3 main requirements are:
+
+    It needs to be able to scan items in by quantity and weight
+    It needs to be able to handle discount coupons… allow for two different types of coupons
+    % off total
+    Buy ‘x’ get ‘y’ free (i.e. buy 3 get 1 free)
+    It should be able to provide a caller with a total cost and a list of items
+ 
+ */
+
 namespace CashRegister
 {
     class Program
